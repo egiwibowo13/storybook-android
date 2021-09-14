cd26d0dba00f7737bf3630789d558606e131c28b
Egi A Wibowo <egiwibowo13@gmail.com>
2021-09-14 12:50:15 +0700

    add story at button component

--- src/stories/button/button.stories.jsx
+++ src/stories/button/button.stories.jsx
@@ -9,19 +9,6 @@ function Template(args) {
 
 export default {
   title: "Material/Button",
-  argTypes: {
-    variant: {
-      control: {
-        type: "select",
-        options: ["contained", "outlined", "text"],
-      },
-    },
-    label: {
-      control: {
-        type: "text",
-      },
-    },
-  },
   parameters: {
     docs: {
       page: function DocPages() {
@@ -31,8 +18,27 @@ export default {
   },
 };
 
-export const ButtonDefault = Template.bind({});
-ButtonDefault.args = {
+export const Standard = Template.bind({});
+Standard.parameters = {
+  controls: { hideNoControlsWarning: true },
+};
+
+export const Controls = Template.bind({});
+Controls.args = {
   label: "hai",
   variant: "contained",
+  loading: false,
+};
+Controls.argTypes = {
+  variant: {
+    control: {
+      type: "select",
+      options: ["contained", "outlined", "text"],
+    },
+  },
+  label: {
+    control: {
+      type: "text",
+    },
+  },
 };
